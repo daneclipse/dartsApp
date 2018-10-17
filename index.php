25bbc8ea4950e3e2904dc437cb559be37f4002d3
daneclipse <dan.t@eclipsedigitalmedia.co.uk>
2018-10-17 17:06:25 +0100

    Linked general css to index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include('connection.php');
 	<title></title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="general.css">
+	<link rel="stylesheet" type="text/css" href="css/general.css">
 </head>
 <body>
 
